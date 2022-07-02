1488f4ff8d7f27a1d6d5b8c91dbc2fdb3135d249
Zdenek Bartozel <Zdenek.Bartozel@bagirasys.com>
2022-07-02 20:55:01 +0200

    Add project files.

---
+++ Kata5/Anagram.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Kata
+{
+    public static List<string> Anagrams(string word, List<string> words)
+    {
+        throw new NotImplementedException();
+    }
+}
