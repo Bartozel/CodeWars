b885c36c577421b28a7c7a82332262ca767257b7
Zdenek Bartozel <Zdenek.Bartozel@bagirasys.com>
2022-09-18 20:15:56 +0200

    remove using

--- Kata4/SnailSort.cs
+++ Kata4/SnailSort.cs
@@ -18,14 +18,6 @@ snail(array) #=> [1,2,3,4,5,6,7,8,9]
 
  * */
 
-
-using System;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace CodeWars
 {
     internal static class SnailSort
